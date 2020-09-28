98def3949b70781adebf36ded39f19d726d644bb
Ricardo Jobse <jobse.ricardo@gmail.com>
2020-09-28 11:37:53 +0200

    finished section 6

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -19,4 +19,8 @@ export class ShoppingListComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onIngredientAdded(ingredient: Ingredient): void {
+    this.ingredients.push(ingredient);
+  }
+
 }
